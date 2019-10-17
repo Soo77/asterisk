1c0a4c70ef604964145aa0f204a0eee45af5c679
yimsb01 <yimsb02@naver.com>
2019-10-17 11:57:06 +0900

    과목, 과외과목, 선생님 도메인 업데이트

---
+++ src/main/java/com/ast/eom/domain/Teacher.java
@@ -0,0 +1,10 @@
+package com.ast.eom.domain;
+
+public class Teacher {
+  private int teacherNo;
+  private String accountNo;
+  private String bankNo;
+  private String lessonCertificate;
+  private String teacherIntro;
+  
+}
