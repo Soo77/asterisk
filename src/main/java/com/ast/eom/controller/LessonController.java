9afcbee48f5e29fdbb09b8de30003546da54d035
Soo77 <shimsh3@naver.com>
2019-11-06 19:58:51 +0900

    수업 시작날짜 끝날짜 시작시간 끝시간 수정기능 구현

--- src/main/java/com/ast/eom/controller/LessonController.java
+++ src/main/java/com/ast/eom/controller/LessonController.java
@@ -1,5 +1,6 @@
 package com.ast.eom.controller;
 
+import java.sql.Date;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.ast.eom.domain.Curriculum;
 import com.ast.eom.domain.CurriculumLessonContents;
 import com.ast.eom.domain.Lesson;
 import com.ast.eom.service.LessonService;
@@ -28,8 +30,6 @@ public class LessonController {
   public void detail(Model model, int lessonNo) throws Exception {
     Lesson lesson = lessonService.lessonDetail(lessonNo);
     String whatDay = lesson.getCurriculum().getCurriculumLessonDay();
-    System.out.println(whatDay);
-    
     String resultDay = "";
     
     if (whatDay.charAt(0) == '1') {
@@ -72,33 +72,45 @@ public class LessonController {
       resultDay = resultDay + "일";
     }
     
-    
-    System.out.println(resultDay);
     lesson.getCurriculum().setCurriculumLessonDay(resultDay);
-    System.out.println(lesson.getCurriculum().getCurriculumLessonDay());
-    
-    
     model.addAttribute("lesson", lesson);
   }
+  
+  
+  
   @RequestMapping("update")
   public String update(
       HttpSession session,
       int[] curriculumLessonNo,
       String[] lessonconts,
-      int[] lessondays
+      int[] lessondays,
+      Date sdt, Date edt, String st, String et
       ) throws Exception {
+    System.out.println("ㅎㅇ");
     
-    int garoNo = (int) session.getAttribute("garo");
+    int garoNo = (int) session.getAttribute("garo");//lessonNo 써서 다시 detail로 돌아가려고
+    
+    Lesson lesson2 = new Lesson();
+    Curriculum curr2 = new Curriculum();
+    lesson2.setLessonNo(garoNo);
+    lesson2.setStartDate(sdt);
+    lesson2.setEndDate(edt);
+    curr2.setCurriculumLessonStartTime(st);
+    curr2.setCurriculumLessonEndTime(et);
+    lesson2.setCurriculum(curr2);
+    lessonService.updateDateAndTime(lesson2);
     
     for (int i = 0; i < lessonconts.length; i++) {
       Lesson lesson = new Lesson();
       CurriculumLessonContents clc = new CurriculumLessonContents();
+      Curriculum curr = new Curriculum();
+      lesson.setLessonNo(garoNo);
       clc.setCurriculumLessonNo(curriculumLessonNo[i]);
       clc.setLessonContents(lessonconts[i]);
       clc.setLessonDays(lessondays[i]);
+      lesson.setCurriculum(curr);
       lesson.setClc(clc);
-      
-      lessonService.update(lesson);
+      lessonService.updateContentsAndDays(lesson);
     }
     
     return "redirect:detail?lessonNo="+garoNo;
