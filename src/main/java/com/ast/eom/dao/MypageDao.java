50d6c4779c5bddbf6d6cf19b1dcb674b58fa1686
yimsb01 <yimsb02@naver.com>
2019-11-11 01:03:00 +0900

    마이페이지 수정 중
    - 주소쪽 flex를 사용하여 반응형 + 간격 조정
    - 주소찾기 버튼 기능
    - 자기소개 textarea 수정 (기존의 원형 박스에 포커스 애니메이션도 추가)
    - 희망과목 추가 및 제거

--- src/main/java/com/ast/eom/dao/MypageDao.java
+++ src/main/java/com/ast/eom/dao/MypageDao.java
@@ -2,16 +2,21 @@ package com.ast.eom.dao;
 
 import java.util.List;
 
+import com.ast.eom.domain.LessonSubject;
 import com.ast.eom.domain.Member;
 import com.ast.eom.domain.Parents;
 import com.ast.eom.domain.School;
 import com.ast.eom.domain.Student;
 import com.ast.eom.domain.Teacher;
+import com.ast.eom.domain.WantedLesson;
 
 public interface MypageDao {
-  Member findBy(int no) throws Exception;
-  Teacher getTeacher(int no) throws Exception;
-  Student getStudent(int no) throws Exception;
-  Parents getParents(int no) throws Exception;
+  Member findBy(int memberNo) throws Exception;
+  Teacher getTeacher(int memberNo) throws Exception;
+  Student getStudent(int memberNo) throws Exception;
+  Parents getParents(int memberNo) throws Exception;
   List<School> getSchools (int no) throws Exception;
+  List<LessonSubject> getLessonSubjects(int memberNo);
+  List<WantedLesson> getWantedLessons(int memberNo);
+  List<Member> getChildrenIdAndName(List<Integer> childrenNo);
 }
