b17f7072390d14afed72948ba74938e4f2e2cf4a
shinwoohyuk <s_swwhh@naver.com>
2019-11-15 12:45:36 +0900

    메세지 서비스 파일 생성

---
+++ src/main/java/com/ast/eom/service/impl/DefaultMessageService.java
@@ -0,0 +1,52 @@
+package com.ast.eom.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ast.eom.dao.MessageDao;
+import com.ast.eom.domain.Member;
+import com.ast.eom.domain.Message;
+import com.ast.eom.service.MessageService;
+
+@Service
+public class DefaultMessageService implements MessageService {
+  
+  @Autowired
+  MessageDao messageDao;
+
+  @Override
+  public List<Member> messageList(int memberNo) throws Exception {
+    return messageDao.messageList(memberNo);
+  }
+
+  @Override
+  public int messageReadShowAll(int memberNo) throws Exception {
+    return messageDao.messageReadShowAll(memberNo);
+  }
+
+  @Override
+  public Integer messageReadShow(Message message) throws Exception {
+    return messageDao.messageReadShow(message);
+  }
+
+  @Override
+  public void messageRead(Message message) throws Exception {
+    messageDao.messageRead(message);
+  }
+
+  @Override
+  public List<Message> detail(int memberNo) throws Exception {
+    return messageDao.detail(memberNo);
+  }
+
+  @Override
+  public List<Message> messageDetail(Message message) throws Exception {
+    return messageDao.messageDetail(message);
+  }
+
+  @Override
+  public int messageIn(Message message) throws Exception {
+    return messageDao.messageIn(message);
+  }
+
+}
